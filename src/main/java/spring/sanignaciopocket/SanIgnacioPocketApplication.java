44d496738516b9c58c2c726af43efd0c37ee1325
alvaritosr <asanchezrubio@safareyes.es>
2025-02-25 12:13:03 +0100

    Primer commit

---
+++ src/main/java/spring/sanignaciopocket/SanIgnacioPocketApplication.java
@@ -0,0 +1,13 @@
+package spring.sanignaciopocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanIgnacioPocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SanIgnacioPocketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/spring/sanignaciopocket/SanIgnacioPocketApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.sanignaciopocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanIgnacioPocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
